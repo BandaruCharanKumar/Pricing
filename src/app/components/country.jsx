888b2c446131523aa51c846745746bdce518a743
charan <charan.bandaru333@gmail.com>
2023-07-12 23:24:49 +0530

    logo changed

--- src/app/components/country.jsx
+++ src/app/components/country.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 
 export default function Country({country,OnChange}){
   return(
-    <div class=" items-center md:ml-auto">
-      <select id="country" class="  m-auto mt-4 p-1  w-[180px] rounded-[28px] border-2 border-[#40ccb3]" onChange={OnChange}>
+    <div class=" items-center text-black md:ml-auto">
+      <select id="country" class="text-black  m-auto mt-4 p-1 w-[180px] rounded-[28px] border-2 border-[#40ccb3]" onChange={OnChange}>
        {country.map((item)=>
-       <option class="rounded-[24px] m-auto text-black" value={item}>{item}</option>)} 
+       <option class="rounded-[24px] text-black m-auto " value={item}>{item}</option>)} 
         {/* <option class="rounded-[24px] m-auto" value="IN">IN</option>
         <option class="rounded-[24px] m-auto" value="US">US</option>
         <option class="rounded-[24px] m-auto" value="KE">KE</option> */}
