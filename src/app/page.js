4a067deb95a69d3bf571e2f67decdf1846c45894
BandaruCharanKumar <135110649+BandaruCharanKumar@users.noreply.github.com>
2023-07-31 15:46:08 +0530

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import MainPrice from "./pricing/mainprice";
+export default function Home() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col bg-[#FFFFFF] ">
+        <MainPrice></MainPrice>
+      </main>
+    </>
+    
+  );
+}
